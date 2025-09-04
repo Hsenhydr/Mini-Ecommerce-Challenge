62f7d642f74e10303deddc1f6245565af1eaf22b
Hsenhydr <Hsenhydr88@gmail.com>
2025-09-04 14:53:27 +0300

    added service -must review-

---
+++ mini-ecommerce/src/main/java/com/example/mini_ecommerce/service/ITokenService.java
@@ -0,0 +1,12 @@
+package com.example.mini_ecommerce.service;
+
+import com.example.mini_ecommerce.entity.User;
+
+// used the help of ai 
+public interface ITokenService {
+    String generateToken(User user);
+
+    boolean validateToken(String token);
+
+    String extractUserEmail(String token);
+}
